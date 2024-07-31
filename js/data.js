5054aff753f865cd42292b6b5ea9755a511028b9
crazy-coder-guy <138527720+crazy-coder-guy@users.noreply.github.com>
2024-07-31 15:02:46 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,35 @@
+var plan = [{
+
+    "id":0,
+},
+{
+
+    "id":1,
+    "plan":"Health",
+    "title":"Health insurance",
+    "price":"",
+    "image": "https://www.hdfclife.com/content/dam/hdfclifeinsurancecompany/knowledge-center/images/health/How-to-Switch-to-a-Different-Health-Insurance-Policy-V2.jpg",
+    "feature":" Covers hospital room rent up to ₹10,000 per day for any room category.Mid-Year Member Addition:Allows addition of a new child to the policy if they are at least 91 days old.Spouses can be added 91 days after policy issuance, provided the wedding date is after the policy issuance date.enewal Bonus:Offers a renewal bonus of ₹1 lakh per year, up to a maximum of an additional ₹5 lakh over the entire duration of the policy.",
+    "description":"The Health Companion Insurance Plan is a comprehensive health insurance policy designed to provide extensive medical coverage and financial protection for individuals and families. This plan offers a host of benefits to ensure that policyholders are well-protected against unforeseen medical expenses and can access quality healthcare without financial stress."
+},
+{
+
+    "id":2,
+    "plan":"Home",
+    "title":"",
+    "price":"",
+    "image": "https://www.hdfcergo.com/images/default-source/home-insurance/importance-of-home-insurance.jpg",
+    "feature":" Covers hospital room rent up to ₹10,000 per day for any room category.Mid-Year Member Addition:Allows addition of a new child to the policy if they are at least 91 days old.Spouses can be added 91 days after policy issuance, provided the wedding date is after the policy issuance date.enewal Bonus:Offers a renewal bonus of ₹1 lakh per year, up to a maximum of an additional ₹5 lakh over the entire duration of the policy.",
+    "description":"The Health Companion Insurance Plan is a comprehensive health insurance policy designed to provide extensive medical coverage and financial protection for individuals and families. This plan offers a host of benefits to ensure that policyholders are well-protected against unforeseen medical expenses and can access quality healthcare without financial stress."
+},
+{
+
+    "id":3,
+    "plan":"Car",
+    "title":"",
+    "price":"",
+    "image": "https://www.icicilombard.com/docs/default-source/apps/fwfresh/fw-landing/images/il-what-car-insurance.svg",
+    "feature":" Covers hospital room rent up to ₹10,000 per day for any room category.Mid-Year Member Addition:Allows addition of a new child to the policy if they are at least 91 days old.Spouses can be added 91 days after policy issuance, provided the wedding date is after the policy issuance date.enewal Bonus:Offers a renewal bonus of ₹1 lakh per year, up to a maximum of an additional ₹5 lakh over the entire duration of the policy.",
+    "description":"The Health Companion Insurance Plan is a comprehensive health insurance policy designed to provide extensive medical coverage and financial protection for individuals and families. This plan offers a host of benefits to ensure that policyholders are well-protected against unforeseen medical expenses and can access quality healthcare without financial stress."
+},
+]
